5010b301b50b160f9fbecda4efda94222b928e1b
Jiri Hysek <jiri.hysek@gmail.com>
2020-04-19 02:19:27 +0200

    Tutorial

--- Components/Exit/Exit.gd
+++ Components/Exit/Exit.gd
@@ -3,8 +3,6 @@ extends Area2D
 onready var game = get_node("/root/Game")
 
 
-
-
 func _on_Exit_body_entered(body):
 	if body.is_in_group("Player"):
 		body.reached_exit()
--- LevelSwitcher.gd
+++ LevelSwitcher.gd
@@ -4,10 +4,12 @@ var current_level = 0
 var levels = [
 	"res://Levels/Level00.tscn",
 	"res://Levels/Level01.tscn",
+	"res://Levels/Level02.tscn",
 	"res://Scenes/Menutscn"
 	]
 
 func restart_level():
+	print("LOADING LEVEL " + levels[current_level])
 	get_tree().change_scene(levels[current_level])
 	
 func next_level():
